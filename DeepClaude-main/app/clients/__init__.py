71e777ff00eb8158df1526105875e927f2bde3d0
131452000000 <103211374+131452000000@users.noreply.github.com>
2025-04-17 01:07:26 +0800

    Delete DeepClaude-main directory

--- DeepClaude-main/app/clients/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base_client import BaseClient
-from .deepseek_client import DeepSeekClient
-from .claude_client import ClaudeClient
-
-__all__ = ['BaseClient', 'DeepSeekClient', 'ClaudeClient']
--- DeepClaude-main/app/manager/__init__.py
+++
@@ -1,5 +0,0 @@
-"""模型管理器包"""
-
-from .model_manager import model_manager
-
-__all__ = ["model_manager"]
